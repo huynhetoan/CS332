def2f071aa5342eb934e25926b416ae5a1b4cd07
Ethan Huynh <152222736+huynhetoan@users.noreply.github.com>
2026-06-22 16:09:46 +0000

    update

---
+++ lab04/README.txt
@@ -0,0 +1,4 @@
+Ethan Huynh
+
+To compile use: gcc -o fileConcat fileConcat.c
+To execute use: ./fileConcat
---
+++ lab04/fileConcat.c
@@ -0,0 +1,5 @@
+/* Ethan Huynh
+To compile use: gcc -o fileConcat fileConcat.c
+To execute use: ./fileConcat
+*/
+
