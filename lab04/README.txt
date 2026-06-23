a1e7ef3937d9c819107563f5231fc521087b1e3f
Ethan Huynh <152222736+huynhetoan@users.noreply.github.com>
2026-06-23 16:28:18 +0000

    update

--- lab04/README.txt
+++ lab04/README.txt
@@ -1,4 +1,15 @@
 Ethan Huynh
+fileConcat.c
+CS 332 - Lab4
 
-To compile use: gcc -o fileConcat fileConcat.c
-To execute use: ./fileConcat
+Create two test file: echo "Hello World! This is file one!" > file1
+                      echo "Hello World! This is file two!" > file2
+
+Check the files: cat file1
+                 cat file2
+
+Compile program: gcc -o fileConcat fileConcat.c
+Execute program: ./fileConcat file1 file2
+
+Check the results: cat file1
+                   cat file2
--- lab04/fileConcat.c
+++ lab04/fileConcat.c
@@ -1,5 +1,69 @@
 /* Ethan Huynh
 To compile use: gcc -o fileConcat fileConcat.c
-To execute use: ./fileConcat
+To execute use: ./fileConcat file1 file2
+Check results: cat file1
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> //open, O_RDONLY, O_WRONLY, O_APPEND flags
+#include <unistd.h> //read, write, close
+#include <string.h>
+
+//buffer for reading chunks (2^10 = 1024)
+#define BUFFSIZE 1024
+
+int main(int argc, char *argv[]) {
+    int fd1, fd2;
+    int n;
+    char buffer[BUFFSIZE];
+
+    //check argument, it must be 3 (fileConcat.c, file1, file2)
+    if (argc != 3) {
+        printf("Usage: %s <file1> <file2>\n", argv[0]);
+        exit(1); //stop program if not 3 arguments
+    }
+
+    //check filename
+    //strcmp checks if the strings are the same
+    if (strcmp(argv[1], argv[2]) == 0) {
+        printf("Error: The two filenames must be different.\n");
+        exit(1); //stop program if files are the same name
+    }
+
+    //open first file in append to add instead of overwriting
+    fd1 = open(argv[1], O_WRONLY | O_APPEND);
+    if (fd1 < 0) {
+        printf("Error opening first file.\n");
+        exit(1); //stop program if unable to open file
+    }
+
+    //open second file in read only
+    fd2 = open(argv[2], O_RDONLY);
+    if (fd2 < 0) {
+        printf("Error opening second file.\n");
+        close(fd1);
+        exit(1);
+    }
+
+    //read file2 and append to file1
+    while ((n = read(fd2, buffer, BUFFSIZE)) > 0) {
+        if (write(fd1, buffer, n) != n) {
+            printf("Write error.\n");
+            close(fd1);
+            close(fd2);
+            exit(1);
+        }
+    }
+
+    //if -1 is returned, there is an error
+    if (n < 0) {
+        printf("Read error.\n");
+    }
+
+    //close files
+    close(fd1);
+    close(fd2);
+
+    return 0;
+}
