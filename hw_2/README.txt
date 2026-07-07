811a3ac043cf7417df99650654288141a4ae260c
Ethan Huynh <152222736+huynhetoan@users.noreply.github.com>
2026-07-07 19:53:38 +0000

    organize

---
+++ hw_2/README.txt
@@ -0,0 +1,9 @@
+Ethan Huynh
+hw2.c
+CS 332 - hw_2
+
+Compile program: gcc -Wall -o hw2 hw2.c
+Execute program: ./hw2
+
+Input:
+Output:
--- hw_2/ethuynh_HW02.c
+++ hw_2/ethuynh_HW02.c
@@ -1,5 +1,5 @@
-/* Ethan Huynh
-To compile use: gcc -Wall -o dirTraverse dirTraverse.c
-To execute use: ./dirTraverse <directory>
+/* Ethan Huynh - hw_2
+To compile use: gcc -Wall -o hw2 hw2.c
+To execute use: ./hw2
 */
 
